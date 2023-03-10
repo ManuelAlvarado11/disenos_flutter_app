3060f87875c3af77ef77ea3607897f502016ac97
ManuelAlvarado <mantonio.alvarado@salud.gob.sv>
2023-03-10 13:51:59 -0600

    :sparkles:Diseño basico 1

--- lib/src/screens/basic_desing.dart
+++ lib/src/screens/basic_desing.dart
@@ -5,11 +5,106 @@ class BasicDesingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-      child: FadeInImage(
-          image: AssetImage('assets/landscape.jpg'),
-          placeholder: AssetImage('assets/landscape.jpg')),
-    ));
+    return Scaffold(
+      body: Column(
+        children: [
+          // Image
+          const FadeInImage(
+            image: AssetImage('assets/landscape.jpg'),
+            placeholder: AssetImage('assets/landscape.jpg'),
+          ),
+
+          // Title
+          const Title(),
+
+          // Buttons section
+          const ButtonSection(),
+
+          // Description
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: const Text(
+                'Fugiat deserunt dolor dolor aute aute aute minim qui consequat qui id. Exercitation excepteur id nostrud est. Exercitation in dolore ex id culpa. Ad exercitation anim amet nisi in veniam officia sint. Sit mollit cupidatat minim occaecat officia duis ea occaecat fugiat cupidatat ea magna. Ullamco nulla culpa proident proident nisi officia.'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+      child: Row(
+        children: [
+          // Descripcion
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text('Este es el titulo de la pantalla',
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              Text('Este es el subtitulo de la pantalla',
+                  style: TextStyle(color: Colors.black45)),
+            ],
+          ),
+
+          // Expanded se expande todo lo que puede dentro de su padre
+          Expanded(child: Container()),
+
+          // Puntaje
+          const Icon(Icons.star, color: Colors.red),
+          const Text('41')
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: const [
+          CustomButton(text: 'Call', icon: Icons.phone),
+          CustomButton(text: 'Route', icon: Icons.room_outlined),
+          CustomButton(text: 'Share', icon: Icons.share)
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final IconData icon;
+
+  const CustomButton({
+    Key? key,
+    required this.text,
+    required this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(icon, color: Colors.blue.shade400),
+        const SizedBox(height: 10),
+        Text(text, style: TextStyle(color: Colors.blue.shade400))
+      ],
+    );
   }
 }
